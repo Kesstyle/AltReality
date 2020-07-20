b4951d2e935cdc6aa406215d4bf458b15e496460
Siarhei Kavalchuk <Siarhei_Kavalchuk@epam.com>
2020-07-20 13:26:01 +0300

    Initial commit

---
+++ src/main/java/by/kes/altReality/AltRealityApplication.java
@@ -0,0 +1,13 @@
+package by.kes.altReality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltRealityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltRealityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/kes/altReality/AltRealityApplicationTests.java
@@ -0,0 +1,13 @@
+package by.kes.altReality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltRealityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
